3a7fc3a44d2790ee41cb6470467cde6fb7591c69
Daniel Uvehag <daniel.uvehag@gmail.com>
2017-08-09 14:51:38 +0200

    Initial support for handling certmonger operations

---
+++ cepces/certmonger/core.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of cepces.
+#
+# cepces is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cepces is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cepces.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""This module contains common shared certmonger classes."""
+
+from enum import IntEnum
+
+
+class Result(IntEnum):
+    """This enumeration contains the result codes expected by certmonger."""
+    EISSUED = 0
+    EWAIT = 1
+    EREJECTED = 2
+    ECONNECTERROR = 3
+    EUNDERCONFIGURED = 4
+    EWAITMORE = 5
+    EUNSUPPORTED = 6
+
+
+class MissingEnvironmentVariable(RuntimeError):
+    """This error is raised when an expected environment variable is missing.
+    """
+    def __init__(self, variable):
+        """Initializes the error.
+
+        :param variable: the name of the missing environment variable.
+        """
+        self._variable = variable
+
+        super().__init__(
+            'The mandatory environment variable {} is missing, '
+            'cannot proceed.'.format(variable)
+        )
+
+        @property
+        def variable(self):
+            """Returns the name of the missing environment variable.
+
+            :return: the name of the missing environment variable.
+            """
+            return self._variable
---
+++ cepces/certmonger/operation.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of cepces.
+#
+# cepces is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cepces is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cepces.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""This module contains all the supported certmonger operations."""
+
+from abc import ABCMeta, abstractmethod
+from cepces.core import Base
+from cepces.certmonger.core import MissingEnvironmentVariable
+import os
+
+
+class Operation(Base, metaclass=ABCMeta):
+    """Abstract base class used by classes mapping certmonger operations.
+
+    As certmonger calls a helper, all of the necessary data is available as
+    environment variables. Some of these variables may be required to perform
+    an action, whereas others are optional. This base class helps ensure
+    everything required is present, or fails otherwise.
+
+    Two class variables are used to define this behaviour:
+
+    * _required_: A list containing all required environment variables.
+    * _optional_: A list containing tuples of all optional environment
+                  variables and their defaults (e.g., '("VAR", None)').
+    """
+    _required_ = []
+    _optional_ = []
+    _name_ = None
+
+    def __init__(self):
+        """Initializes an Operation.
+
+        All required and optional environment variables are verified, read and
+        stored in the instance as a dictionary.
+        """
+        super().__init__()
+
+        self._vars = {}
+
+        # Verify that all required environment variables are present.
+        for var in self.__class__._required_:
+            if var not in os.environ:
+                raise MissingEnvironmentVariable(var)
+            else:
+                self._vars[var] = os.environ[var]
+
+        # Get all optional variables and set their defaults if they're missing.
+        for var, default in self.__class__._optional_:
+            if var not in os.environ:
+                self._vars[var] = default
+            else:
+                self._vars[var] = os.environ[var]
+
+    @abstractmethod
+    def __call__(self):
+        """Calls the operation to let it performs its logic."""
+        pass
+
+    @property
+    def name(self):
+        """Returns the operation's name as designated by certmonger.
+
+        :return: The certmonger operation's name.
+        """
+        if not self.__class__._name_:
+            raise RuntimeError('No name has been set for {}'
+                               .format(self.__class__))
+
+    @property
+    def result(self):
+        """Returns the final result of the operation."""
+        # If no result has been set, raise an exception. Either the result is
+        # requested too early, or it is never set.
+        if not hasattr(self, '_result'):
+            raise RuntimeError('No result has been set')
+        else:
+            return self._result
