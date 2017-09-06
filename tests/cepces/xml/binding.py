b13d703922458a82fe60fdd7efc5528984ab0c00
Daniel Uvehag <daniel.uvehag@gmail.com>
2017-09-06 12:42:27 +0200

    Fixed erroneous test imports

--- tests/cepces/xml/binding.py
+++ tests/cepces/xml/binding.py
@@ -16,8 +16,8 @@
 # along with cepces.  If not, see <http://www.gnu.org/licenses/>.
 #
 import unittest
-from cepces.binding import ListingMeta
-from cepces.binding import XMLDescriptor
+from cepces.xml.binding import ListingMeta
+from cepces.xml.binding import XMLDescriptor
 
 
 class MockXMLDescriptor(XMLDescriptor):
