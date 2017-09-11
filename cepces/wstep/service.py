def9ee7fcf943fa713e304ff44398277549eeb26
Daniel Uvehag <daniel.uvehag@gmail.com>
2017-09-11 14:12:31 +0200

    Add missing WSTEP files

---
+++ cepces/wstep/service.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of cepces.
+#
+# cepces is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cepces is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cepces.  If not, see <http://www.gnu.org/licenses/>.
+#
+from cepces.soap.service import Service as SOAPService
+from cepces.soap.types import Envelope
+from cepces.wstep.types import SecurityTokenRequest
+from cepces.wstep.types import SecurityTokenResponseCollection
+from xml.etree import ElementTree
+import uuid
+import re
+
+ACTION = 'http://schemas.microsoft.com/windows/pki/2009/01/enrollment/' \
+         'RST/wstep'
+
+
+class Service(SOAPService):
+    def _get_envelope(self, payload):
+        envelope = Envelope()
+        envelope.header.action = ACTION
+        envelope.header.message_id = "urn:uuid:{0:s}".format(str(uuid.uuid4()))
+        envelope.header.to = self._endpoint
+        envelope.body.payload = payload._element
+
+        self._logger.debug(
+            "Preparing message {} to {} with payload: {}".format(
+                envelope.header.message_id,
+                envelope.header.to,
+                ElementTree.tostring(payload._element),
+            )
+        )
+
+        return envelope
+
+    def request(self, csr):
+        m = re.search('^\-{5}BEGIN NEW CERTIFICATE REQUEST\-{5}\n'
+                      '(.*)\n'
+                      '\-{5}END NEW CERTIFICATE REQUEST\-{5}\s*$',
+                      csr,
+                      flags=re.DOTALL)
+
+        if not m:
+            raise LookupError("Invalid CSR.")
+
+        token = SecurityTokenRequest()
+        token.token = m.group(1)
+
+        envelope = self._get_envelope(token)
+        result = self.send(envelope)
+
+        # if result is None:
+        #     raise LookupError('No responses.')
+        soap_response = Envelope(result)
+        response = SecurityTokenResponseCollection(soap_response.body.payload)
+
+        # All responses has to be processed before hand, since they need to be
+        # curated of any (possible) extra Microsoft-added line endings.
+        for r in response.responses:
+            r.token.text = r.token.text.replace('&#xD;', '')
+
+            return response.responses
