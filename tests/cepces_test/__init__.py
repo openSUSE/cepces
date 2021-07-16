de5bad6a412eba6c7cd40d75b045d44d68a61dcd
David Mulder <dmulder@suse.com>
2021-07-16 10:48:00 -0600

    Add a test runner for executing unittests

--- tests/cepces_test/__init__.py
+++ tests/cepces_test/__init__.py
@@ -15,3 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with cepces.  If not, see <http://www.gnu.org/licenses/>.
 #
+from .certmonger import *
+from .xcep import *
+from .xml import *
--- tests/cepces_test/xml/__init__.py
+++ tests/cepces_test/xml/__init__.py
@@ -15,3 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with cepces.  If not, see <http://www.gnu.org/licenses/>.
 #
+from .binding import *
+from .converter import *
+from .util import *
